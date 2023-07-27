12239b6cafcc3580aa3933fcbaa9db447bfce809
agileago <agileago@gmail.com>
2023-07-27 11:49:28 +0800

    fix: 新版 src 下面必须有 html

--- src/module/demo/first/index.tsx
+++ src/module/demo/first/index.tsx
@@ -1,8 +1,8 @@
 import { VueComponent } from 'vue3-oop'
-import { Button } from '@nutui/nutui-taro'
 import { MiniHook } from '@vue3-oop/taro-hooks'
 import { abcRequest } from '@/api/http'
-import Taro, { useRouter } from '@tarojs/taro'
+import { useRouter } from '@tarojs/taro'
+import { Button } from '@tarojs/components'
 
 export default class First extends VueComponent {
   router = useRouter()
@@ -11,17 +11,16 @@ export default class First extends VueComponent {
     abcRequest('/abc', { method: 'get', params: { a: 1 } }).then(res => console.log('res', res))
   }
 
+  handlePhone() {
+    console.log(111)
+  }
+
   render() {
     return (
       <div class={'text-amber-300'}>
         <div class={'mt-[22px] text-center text-[50px] text-red-400'}>aaa</div>
-        <Button
-          type={'primary'}
-          block
-          class={'text-xl'}
-          onClick={() => Taro.navigateTo({ url: '/module/demo/two/index' })}
-        >
-          first
+        <Button openType={'getPhoneNumber'} onGetphonenumber={() => this.handlePhone()}>
+          11111
         </Button>
         <view key={1}></view>
       </div>
