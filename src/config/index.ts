93870ea569cff8c372c52e2cd88e7edaf901dedd
agileago <agileago@gmail.com>
2023-08-25 11:11:43 +0800

    feat: update taro version

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,12 @@
 import Config from '@/config/config.default'
 
-let TargetConf = Config
-// 自动扫描配置文件
-const requireContext = require.context('./', false, /config\.(\w+)\.ts$/)
-requireContext.keys().forEach(path => {
-  if (path !== `./config.${process.env.VUE_APP_MODE}.ts`) return
-  TargetConf = requireContext(path).default
-})
+const TargetConf = Config
+// 自动扫描配置文件 此处因为taro bug暂时不放开
+// const requireContext = require.context('./', false, /config\.(\w+)\.ts$/)
+// requireContext.keys().forEach(path => {
+//   if (path !== `./config.${process.env.VUE_APP_MODE}.ts`) return
+//   TargetConf = requireContext(path).default
+// })
 
 const config = new TargetConf()
 
